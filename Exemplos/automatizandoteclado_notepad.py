91c98e8121c89ae1d83521ab642522e494dcfeea
Alan de Oliveira Gonçalves <105790117+Alan-oliveir@users.noreply.github.com>
2023-10-29 12:55:25 -0300

    Add files via upload

---
+++ Exemplos/automatizandoteclado_notepad.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+pyautogui.hotkey("win", "r")
+pyautogui.write("notepad")
+pyautogui.press("enter")
+
+time.sleep(3)
+
+pyautogui.typewrite("Hello world")
+pyautogui.press("enter")
+pyautogui.typewrite("Sejam bem vindos")
