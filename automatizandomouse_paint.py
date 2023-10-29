7f278364b570f950b22f67c9aaf6112462355c13
Alan de Oliveira Gonçalves <105790117+Alan-oliveir@users.noreply.github.com>
2023-10-29 11:22:57 -0300

    Update automatizandomouse_paint.py

--- automatizandomouse_paint.py
+++ automatizandomouse_paint.py
@@ -1,15 +1,15 @@
 import pyautogui
 
-#configurar um tempo entre as ações
+# Configurar um tempo entre as ações
 pyautogui.PAUSE = 2.5
 
-#Automatizar o meu teclado
-#utilizar funções capazes de abrir o Paint
+# Automatizar o meu teclado
+# Utilizar funções capazes de abrir o Paint
 pyautogui.hotkey("win","r")
 pyautogui.write("mspaint")
 pyautogui.press("enter")
 
-#mova o mouse para a posição e clique(seleciona o lápis)
+# Mova o mouse para a posição e clique(seleciona o lápis)
 x,y =(300,125)
 pyautogui.click(x,y)
 
@@ -18,19 +18,12 @@ pyautogui.PAUSE = 2.0
 
 distance = 400
 while distance > 0:
-    #movendo mouse para a direita
+    # Movendo mouse para a direita
     pyautogui.dragRel(distance,0,duration=0.5)
     distance -= 25
-    #move o mouse para baixo
+    # Move o mouse para baixo
     pyautogui.dragRel(0,distance,duration=0.5)
-    #move o mouse para a esquerda
+    # Move o mouse para a esquerda
     pyautogui.dragRel(-distance,0,duration = 0.5)
     distance -=25
-    pyautogui.dragRel(0,-distance,duration = 0.5 )
-
-
-
-
-
-
-
+    pyautogui.dragRel(0,-distance,duration = 0.5)
--- automatizandoteclado_notepad.py
+++ automatizandoteclado_notepad.py
@@ -6,8 +6,7 @@ pyautogui.write("notepad")
 pyautogui.press("enter")
 
 time.sleep(3)
-pyautogui.press("enter")
+
 pyautogui.typewrite("Hello world")
 pyautogui.press("enter")
 pyautogui.typewrite("Sejam bem vindos")
-
